b3deda0a4548bf9e6411d9ab445814cc3a1ef033
Elson Otake <elsonotake@yahoo.com.br>
2022-11-02 16:03:17 -0300

    Add Datalist

--- src/App.js
+++ src/App.js
@@ -1,57 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import TreeLeaf from './components/TreeLeaf';
 import './App.css';
 
+const state = {
+  tree: [
+    {
+      id: 1,
+      name: 'Elson Akio Otake',
+    },
+    {
+      id: 2,
+      name: 'Lilian Hiromi Job',
+    },
+    {
+      id: 3,
+      name: 'Tomoyo Job',
+    }
+  ]
+}
+
 function App() {
+  const onClick = () => {
+    return (
+      <div>
+        <TreeLeaf />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <h1>Family tree</h1>
+      <form>
+        <label className='form-label'>Choose a name from the list</label>
+        <input className="form-control" list="names" name="name" id="FamilyTree" placeholder="Type to search..."/>
+        <datalist key={state.tree.id} id="names">
+          {state.tree.map(t => (
+            <option value={t.name} />
+          ))}
+        </datalist>
+        <input type="submit" onClick={onClick}/>
+      </form>
+    </>
   );
 }
 
---
+++ src/components/TreeLeaf.js
@@ -0,0 +1,9 @@
+import React from "react"
+const TreeLeaf = () => {
+  return (
+    <div>
+      <h1>Hello from Tree Leaf</h1>
+    </div>
+  )
+}
+export default TreeLeaf;
