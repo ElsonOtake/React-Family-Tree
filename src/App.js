504799a987bba5aa6c60cd84e93dbe5ce2b6cfcd
Elson Otake <elsonotake@yahoo.com.br>
2022-11-16 00:11:38 -0300

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Details from './components/Details';
 
-function App() {
-  return (
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:id" element={<Details />} />
-        </Routes>
-      </Router>
-    </React.StrictMode>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
