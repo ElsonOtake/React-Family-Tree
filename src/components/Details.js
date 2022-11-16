e3351f2bba8cf1f7f3fc2da533b0a234a9d33115
Elson Otake <elsonotake@yahoo.com.br>
2022-11-16 00:13:21 -0300

    Update to receive data from reducer

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { fetchLeaf } from '../redux/Details/Details';
 
 const Details = () => {
+  const leafData = useSelector((state) => state.leafReducer);
+  const dispatch = useDispatch();
   const { id } = useParams();
-  // const requestURL = `https://elsonotake-backend.herokuapp.com/api/v1/users`;
-  const requestURL = `http://localhost:3000/api/v1/users`;
-  const request = new Request(requestURL);
-  fetch(request)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
+  useEffect(() => {
+    dispatch(fetchLeaf(id));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
       <h1>Hello from Tree Leaf ({id})</h1>
+      <p>Id: {leafData.id}</p>
+      <p>Name: {leafData.name}</p>
+      <p>Gender: {leafData.gender}</p>
+      <p>Description: {leafData.description}</p>
+      <p>Birth: {leafData.birth}</p>
+      <p>Death: {leafData.death}</p>
+      <p>Alive: {leafData.alive}</p>
     </div>
   )
 }
