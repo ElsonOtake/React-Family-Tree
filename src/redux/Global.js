17d3fc820f5e0df55867173f3a27e52c3cd297d9
Elson Otake <elsonotake@yahoo.com.br>
2022-12-04 17:21:28 -0300

    Add deployed address

--- src/redux/Global.js
+++ src/redux/Global.js
@@ -1,3 +1,4 @@
+// const urlFamilyTree = 'https://elsonotake-family-tree-backend.onrender.com/api/v1/users';
 const urlFamilyTree = 'http://localhost:3000/api/v1/users';
 
 export default urlFamilyTree;
